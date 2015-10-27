8e9e3cf6a20ceddc3406cc9cad282136140a045f
Andrew Tarr <andrew.tarr@hotmail.com>
2015-10-27 11:22:06 -0500

    Initial code commit

---
+++ CustomClaimsIdentityMVC/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomClaimsIdentityMVC.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected bool IsAnonymousUser()
+        {
+            var claim = ((ClaimsIdentity)User.Identity).Claims.FirstOrDefault(c => c.Type == "urn:Custom:UserType");
+            return claim.Value == "AnonymousUser";
+        }
+    }
+}
---
+++ CustomClaimsIdentityMVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using CustomClaimsIdentityMVC.Controllers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdentitySample.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Congratulations, you're authorized!";
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Contact()
+        {
+            if (IsAnonymousUser())
+            {
+                HttpContext.GetOwinContext().Authentication.SignOut();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+}
